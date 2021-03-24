c857a4afcc03e8672d2d233f85d71077bef81c91
RJ2506 <77298403+RJ2506@users.noreply.github.com>
2021-03-24 15:36:15 -0700

    added the clone to html

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,18 @@
-// your JavaScript goes here, you can delete this comment
+
+const todolist = [
+                    {todo: "clone repo for starter code", status:"complete" },
+                    {todo: "added JS src in html", status: "complete"}, 
+                    {todo: "creating JS in html", status: "started"}, 
+                    {todo: "push the file into the remote" , status: "started"}
+                 ]
+
+const completedTodos = []
+
+for (task of todolist) {
+    if (task['status'] === "complete"){
+        completedTodos.push(task)}
+}
+
+for (value of completedTodos) {
+    console.log(value['todo'])
+}
